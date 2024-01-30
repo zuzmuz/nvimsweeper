836d5cc3a956d6f3a7e8cbda5d71c201bf1dec7b
zahersunique <zaher@bandindustries.com>
2024-01-30 10:26:32 +0200

    add plugin folder

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local Grid = require("game.grid")
 
 local grid = Grid.new(10, 10, 10)
 
-Grid.init(grid, 1, 1)
+Grid.init(grid, 4, 2)
 
 for i = 1, grid.width*grid.height do
     if grid.cells[i].value == -1 then
